7197e73de40551233de9f7c8327769b4908d6199
Marcin Moskal <minemoskas@gmail.com>
2020-08-20 11:21:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,9 @@
 
 // Potrzebne biblioteki
-const { Client, MessageAttachment, MessageEmbed } = require('discord.js');
-const Discord = require('discord.js');
+const { Client, MessageAttachment, MessageEmbed } = require('discord.js');;
 const bot = new Client();
-//const client = require('discord-rich-presence')('526893482309320705');
-const user = ('291735895986405386');
 const fs = require('fs');
+const { timeStamp } = require('console');
 // Dodawanie wykrywania prefixu dla bota
 
 var pre='*';
@@ -16,16 +14,26 @@ function commandIs(str,msg){
 // Log do konsoli po zalogowaniu się do serwerów discorda.
 bot.on('ready', () => {
   console.log('Udało się zalogować jako ' + `${bot.user.username}`);
-//  time(); //Funkcja pobierajaca czas systemowy
-//  rich(); //Inicjalizacja rich presence
-//  console.log('O godzinie '+ h +":"+ minuty +":" + sek + " | " + dz +" "+ mies );
+  time(); //Funkcja pobierajaca czas systemowy
+  console.log('O godzinie '+ h +":"+ minuty +":" + sek + " | " + dz +" "+ mies );
   bot.user.setActivity('around with *help');
 });
 
-var komendy =
-[
-  '```Lista aktualnie dostępnych komend (Prefix *)\n\nhelp - wyświetla tą liste komend\navatar - podaje link do twojego avatara i wstawia go jako obrazek\nroll - losuje liczbę z zakresu 1-6 (tak jak kostka lol)\nsay [argument] - bot powtarza napisany argument\ntime - podaje aktualna godzine i datę\nquote - bot podaje losowy cytat\nimg - losowy obrazek```'
-];
+function time(){
+	var d = new Date();h = d.getHours();minuty = d.getMinutes();sek = d.getSeconds();dz = d.getDate();mies = d.getMonth();
+              // Ustawianie miesiecy
+              switch(mies){case 0: mies = 'Styczeń'; break;case 1: mies = 'Luty';break;case 2: mies = 'Marzec';break;case 3: mies = 'Kwiecień';break;case 4: mies = 'Maj';break;case 5: mies = 'Czerwiec';break;case 6: mies = 'Lipiec';break;
+              case 7: mies = 'Sierpień';break;case 8: mies = 'Wrzesień';break;case 9: mies = 'Październik';break;case 10: mies = 'Listopad';break;case 11: mies = 'Grudzień';break;
+              }
+              // Zamiana minut i sekund z 1 na 01
+              switch(minuty){case 0: minuty = '00';break;case 1: minuty = '01';break;case 2: minuty = '02';break;case 3: minuty = '03';break;case 4: minuty = '04';break;case 5: minuty = '05';break;case 6: minuty = '06';break;
+              case 7: minuty = '07';break;case 8: minuty = '08';break;case 9: minuty = '09';break;}
+              switch(sek){case 0: sek = '00';break;case 1: sek = '01';break;case 2: sek = '02';break;case 3: sek = '03';break;case 4: sek = '04';break;case 5: sek = '05';break;case 6: sek = '06';break;
+              case 7: sek = '07';break;case 8: sek = '08';break;case 9: sek - '09';break;}
+              switch(h){
+                case 0: h = '00';
+			  }
+}
 
 var cytaty =['Brak'];
 
@@ -68,10 +76,10 @@ var heavy_115T=[type[0]];
 //Special craft
 {
 var timer_special=['0:11:00','0:12:00','0:13:00','0:20:00','0:24:00','0:30:00','0:34:00','0:35:00','2:00:00','2:10:00','2:15:00','2:20:00','2:22:00','2:30:00','2:40:00','2:47:00','3:00:00','3:40:00','3:50:00',"4:00:00"];
-var special_11=['U-47','U-110','U-552','U-73'];
+var special_11=['U-47','U-110','U-522','U-73'];
 var special_11R=[rarity[3],rarity[4],rarity[4],rarity[4]];
 var special_12=['U-101','U-81'];
-var special_12R=[rarity[4],rarity[4]];
+var special_12R=[rarity[4],rarity[3]];
 var special_13=['U-556',"U-557"];
 var special_13R=[rarity[4],rarity[4]];
 var special_20=['Minato_Aqua'];
@@ -109,6 +117,9 @@ var special_350R=[rarity[4]];
 var special_400=['Ark_Royal'];
 var special_400R=[rarity[4]];
 }
+
+//PR
+let Cheshire=['Cheshire','PR3','CA','https://azurlane.koumakan.jp/w/images/2/2e/CheshireChibi.png',"I am a heavy cruiser of the Royals, Chesire! But before I met darling, I am a ship that only existed in one's imagination. So, this body of mine, belongs to you, Darling!",'5141'];
 
 // Oczekiwanie na wiadomość
 
@@ -253,6 +264,21 @@ function heavycraft(){
         ship_rarity=special_240R[ship];
         ship_name=special_240[ship];
         break;
+    case 15: 
+        ship=Math.floor(Math.random() * special_247.length)
+        ship_rarity=special_247R[ship];
+        ship_name=special_247[ship];
+        break;
+    case 16: 
+        ship=Math.floor(Math.random() * special_300.length)
+        ship_rarity=special_300R[ship];
+        ship_name=special_300[ship];
+        break;
+    case 17: 
+        ship=Math.floor(Math.random() * special_340.length)
+        ship_rarity=special_340R[ship];
+        ship_name=special_340[ship];
+        break;
     }
     ship_img=ship_name+'.png';
     return timer,ship_name;
@@ -271,12 +297,21 @@ function heavycraft(){
 
 bot.on('message', msg => {
     if(commandIs("help",msg)){
-      msg.channel.send(komendy)
+      var komendy;
+      fs.readFile('./data/help.txt','utf8',function(err,komendy){
+      msg.channel.send(komendy);
+      });
     }
 
     if(commandIs("quote",msg)){
       msg.channel.send((cytaty[Math.floor(Math.random() * cytaty.length-1)]));
     }
+
+    if(commandIs("time",msg)){
+      time();
+      msg.channel.send('aktualnie jest: '+ h +":"+ minuty +":" + sek + " | " + dz +" "+ mies );
+    }
+
 
     if(commandIs("avatar",msg)){
       const avatar= new MessageAttachment(msg.author.avatarURL());
@@ -303,6 +338,7 @@ bot.on('message', msg => {
       .setTitle(ship_name)
       .setDescription('Timer: ' + timer_light[timer])
       .addField('Rarity', ship_rarity, true)
+      .addField('Type', ship_type, true)
       .setColor(r_color)
       .attachFiles([attach])
       .setImage(image)
@@ -339,6 +375,7 @@ bot.on('message', msg => {
             .setTitle(ship_name)
             .setDescription('Timer: ' + timer_special[timer])
             .addField('Rarity', ship_rarity, true)
+            .addField('Type', ship_type, true)
             .setColor(r_color)
             .attachFiles([attach])
             .setImage(image)
@@ -442,6 +479,24 @@ bot.on('message', msg => {
               .setTimestamp();
               msg.channel.send(embed);
             };break;
+            case 'test':{
+                const attach='./img/ships/'+Cheshire[0]+'.png';
+                const image='attachment://'+Cheshire[0]+'.png';
+                console.log(attach+' '+image);
+              const embed = new MessageEmbed()
+              .setTitle(Cheshire[0]+' '+Cheshire[1])
+              .setURL('https://azurlane.koumakan.jp/'+Cheshire[0])
+              .setDescription(Cheshire[4])
+              .addField('Rarity', 'Priority', true)
+              .addField('Type', Cheshire[2],true)
+              .setColor('0xffee00')
+              .attachFiles([attach])
+              .setThumbnail(Cheshire[3])
+              .setImage(image)
+              .setFooter('Moskas','https://moskas.github.io/IMG/icon.png')
+              .setTimestamp();
+              msg.channel.send(embed);
+            }break;
             default:msg.reply('Nie ma takiego statku');break;
           }
         }
